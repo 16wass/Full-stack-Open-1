9012c039497ce9330a6925f5baf355170df230e3
wassim mezghanni <ge27tuv@mytum.de>
2024-06-14 18:22:05 +0200

    4.17: Blog List Expansion, step 5

--- Part4/bloglist/controllers/blogs.js
+++ Part4/bloglist/controllers/blogs.js
@@ -1,8 +1,29 @@
-const express = require('express');
-const blogsRouter  = express.Router();
+const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
+blogsRouter.post('/', async (request, response) => {
+  const { title, author, url, likes } = request.body;
 
+  const users = await User.find({});
+  const user = users[0]; // Simplified: Use the first user found
+
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes,
+    user: user._id
+  });
+
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
+  response.status(201).json(savedBlog);
+});
+
+/*
 blogsRouter .get('/', async (request, response) => {
   const blogs = await Blog.find({});
   response.json(blogs);
@@ -19,7 +40,7 @@ const blog = new Blog({
   url,
   author,
   likes: likes || 0
-});
+});*/
 // 4.13 Blog List Expansions, step 1
 blogsRouter.delete('/:id', async (request, response) => {
   const { id } = request.params;
@@ -50,10 +71,14 @@ blogsRouter.put('/:id', async (request, response) => {
   }
 });
 
-
-const savedBlog = await blog.save();
-response.statuts(201).json(savedBlog);
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
+  response.json(blogs);
 });
-  
+
 module.exports = blogsRouter;
+
+
+
+
   
