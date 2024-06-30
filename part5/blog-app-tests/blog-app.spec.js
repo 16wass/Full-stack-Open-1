1554b9f5bd5e94681786eb5a642b673acd8c5adf
wassim mezghanni <ge27tuv@mytum.de>
2024-06-30 19:50:10 +0200

    5.18: Blog List End To End Testing, step 2

--- part5/blog-app-tests/blog-app.spec.js
+++ part5/blog-app-tests/blog-app.spec.js
@@ -1,7 +1,13 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { reset, createUser } = require('./backend-api');
 
 describe('Blog app', () => {
+    let user;
   beforeEach(async ({ page }) => {
+
+    await reset(); 
+    /** user  */
+    user = await createUser();
     await page.goto('http://localhost:5173')
   })
 
@@ -9,5 +15,31 @@ describe('Blog app', () => {
     await expect (page).toHaveText('form > input[name="username"]');
     await expect (page).toHaveText('form > input[name="password"]');
     await expect (page).toHaveText('form > button');
-  })
+  });
+
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.fill('input[name="username"]', user.username);
+      await page.fill('input[name="password"]', user.password);
+
+      // form submission
+      await Promise.all([
+        page.waitForNavigation(), 
+        page.click('form > button[type="submit"]'),
+      ]);
+
+      await expect(page).toHaveText(`Logged in as ${user.name}`);
+    });
+
+    test('fails with wrong credentials', async ({ page }) => {
+      // a login form with incorrect credentials
+      await page.fill('input[name="username"]', 'invalidusername');
+      await page.fill('input[name="password"]', 'invalidpassword');
+
+     //form submission
+      await page.click('form > button[type="submit"]');
+      await expect(page).toHaveText('Wrong username or password. Please try again.');
+    });
+  });
 })
