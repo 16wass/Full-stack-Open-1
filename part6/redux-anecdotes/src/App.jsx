b1bb6b6af5742085c895fc6b981303f6f4f44800
wass16 <wassim.wmez@gmail.com>
2025-03-04 14:17:58 +0100

    6.9 Better Anecdotes, step 7

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,10 +1,12 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import AnbecdoteFilter from './components/AnecdoteFilter'
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <AnbecdoteFilter />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,18 +2,23 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) =>
-    state.slice().sort((a, b) => b.votes - a.votes) // Sort by votes
-  )
+    const { anecdotes, filter } = useSelector((state) => ({
+        anecdotes: state.anecdotes,
+        filter: state.filter,
+      }))
+
+      const filteredAnecdotes = anecdotes.filter((anecdote) =>
+        anecdote.content.toLowerCase().includes(filter.toLowerCase())
+      )
   const dispatch = useDispatch()
 
   const vote = (id) => {
-    dispatch(voteAnecdote(id)) // Dispatch the voteAnecdote action
+    dispatch(voteAnecdote(id))
   }
 
   return (
     <div>
-      {anecdotes.map((anecdote) => (
+      {filteredAnecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
